354d14bd0a2c8be5f60330cc69c82d20a2999277
Varun Rohit Vadapalli <105191986+Varun-298@users.noreply.github.com>
2022-06-03 19:33:57 +0530

    Add files via upload

---
+++ Play_With_Lambda.py
@@ -0,0 +1,5 @@
+# Play with Lambda
+# Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+output = lambda input1 : input1 + 25
+print("Your Output is",output(int(input("Please provide an input value:"))))
